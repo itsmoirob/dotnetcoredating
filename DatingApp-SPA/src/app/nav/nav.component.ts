c04c576f080385979e4bee9f4cc466879c109431
Robbie <rob.w.sherman@gmail.com>
2018-11-30 11:51:11 +0000

    Add routing

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
+
 import { AlertifyService } from '../_services/alertify.service';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -12,27 +14,40 @@ export class NavComponent implements OnInit {
 
   constructor(
     protected authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) { }
 
   ngOnInit() {
   }
 
+  /**
+   * will log a user in
+   */
   login(): void {
     this.authService.login(this.model)
       .subscribe(next => {
         this.alertify.success('Logged in successfully.')
       }, error => {
         this.alertify.error(error);
+      }, () => {
+        this.router.navigate(['/members']);
       });
   }
 
+  /**
+   * Will check if a user is logged in 
+   */
   loggedIn() {
     return this.authService.loggedIn();
   }
 
+  /**
+   * Will log a use out
+   */
   logout() {
     localStorage.removeItem('token');
+    this.router.navigate(['/home']);
     this.alertify.message('Logged out.');
   }
 
